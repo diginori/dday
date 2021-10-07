72a40b353cd767a10c925eaf258c1d11a164e1fa
gyeongguk-min <diginori.min@navercorp.com>
2021-10-07 12:56:53 +0900

    Google Analytics

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = 2021,
-      dayMonth = "11/24/",
+      dayMonth = "9/24/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
